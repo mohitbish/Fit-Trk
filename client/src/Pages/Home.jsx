58f37b44dac443f7ef5db29bb158615651707e7b
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-07-03 06:31:02 +1000

    work on home page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,26 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import Infoform from "../Components/Infoform";
 
 const Home = () => {
   const navigate = useNavigate();
-  const [user, setuser] = useState({});
-  console.log(user)
+  const [username, setusername] = useState("");
+  const [userht, setuserht] = useState(undefined);
+  const [infocheck, setinfocheck] = useState(true);
+
   useEffect(() => {
     (async () => {
       if (!localStorage.getItem("current-user")) {
         navigate("/");
       } else {
-        setuser(
-          await JSON.parse(localStorage.getItem("current-user"))
+        setusername(
+          await JSON.parse(localStorage.getItem("current-user")).username
         );
-        
+        setuserht(
+          await JSON.parse(localStorage.getItem("current-user")).height
+        );
+        console.log("1");
       }
     })();
-  }, [navigate]);
+  }, []);
+
+  useEffect(() => {
+    console.log(userht);
+    if (userht !== 0) {
+      setinfocheck(!infocheck);
+    }
+  }, [username]);
 
   return (
-    <div>
-      <h1>hello {user.username} </h1>
+    <div className="w-[100vw] h-[100vh] flex flex-row bg-[#202124]">
+      <div className="w-3/5">
+        <h1>bkjnlfck</h1>
+      </div>
+      <div className="w-2/5">
+      <h1 className="uppercase text-center text-white">
+        Hello {username}!
+      </h1>
+        {infocheck ? <p>fgvhjbk</p> : <Infoform />}
+      </div>
     </div>
   );
 };
