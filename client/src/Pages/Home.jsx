55b687ffaef2ff0bcc627812125d698a93a477cb
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-07-04 19:45:50 +1000

    EXERCISE API WORKING

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -9,6 +10,7 @@ const Home = () => {
   const [gender, setGender] = useState("");
   const [activity, setactivity] = useState("");
   const [calories, setCalories] = useState(0);
+  const [workout,setworkout]=useState([{}]);
 
   useEffect(() => {
     setusername(JSON.parse(localStorage.getItem("current-user")).username);
@@ -42,62 +44,92 @@ const Home = () => {
     console.log(calories);
   }, [username, calories]);
 
+  useEffect(() => {
+    (async () => {
+      const res = await axios({
+        method: "GET",
+        url: "https://api.api-ninjas.com/v1/exercises?muscle=biceps&type=strength",
+        headers: { "X-Api-Key": "5Gi2Spg6yLQb9wyKC4zKGA==dnbXycfZszOK3KAf" },
+        contentType: "application/json",
+      });
+      setworkout(res.data)
+    })(); 
+  }, []);
+
   return (
     <div className="w-[100vw] h-[100vh] flex sm:flex-row bg-[#202124]">
       <div className="sm:w-3/5">
-        <h1 >Workouts</h1>
+        {workout.map((e,index) => (<h1 key={index}  className="text-white">{e.name}</h1>))}
+        
       </div>
       <div className="sm:w-2/5 flex flex-col items-center">
         <h2 className="uppercase text-center text-white mt-10">
           Calorie Information
         </h2>
         <table className="min-w-full border border-gray-500 my-4  text-white">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b border-gray-500">Goals</th>
-            <th className="py-2 px-4 border-b border-gray-500">Calories</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="py-2 px-4 border-b border-gray-500">Maintain Weight</td>
-            <td className="py-2 px-4 border-b border-gray-500">{calories} cal</td>
-          </tr>
-          <tr>
-            <td className="py-2 px-4 border-b border-gray-500">Lose Weight</td>
-            <td className="py-2 px-4 border-b border-gray-500">{calories-100} cal</td>
-          </tr>
-          <tr>
-            <td className="py-2 px-4 border-b border-gray-500">Gain Weight</td>
-            <td className="py-2 px-4 border-b border-gray-500">{calories+100} cal</td>
-          </tr>
-        </tbody>
-      </table>
-      <h2 className="uppercase text-center text-white mt-10">
-          Dialy Macros
-        </h2>
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border-b border-gray-500">Goals</th>
+              <th className="py-2 px-4 border-b border-gray-500">Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="py-2 px-4 border-b border-gray-500">
+                Maintain Weight
+              </td>
+              <td className="py-2 px-4 border-b border-gray-500">
+                {calories} cal
+              </td>
+            </tr>
+            <tr>
+              <td className="py-2 px-4 border-b border-gray-500">
+                Lose Weight
+              </td>
+              <td className="py-2 px-4 border-b border-gray-500">
+                {calories - 100} cal
+              </td>
+            </tr>
+            <tr>
+              <td className="py-2 px-4 border-b border-gray-500">
+                Gain Weight
+              </td>
+              <td className="py-2 px-4 border-b border-gray-500">
+                {calories + 100} cal
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <h2 className="uppercase text-center text-white mt-10">Dialy Macros</h2>
         <table className="min-w-full border border-gray-500 my-4  text-white">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b border-gray-500">Macro</th>
-            <th className="py-2 px-4 border-b border-gray-500">Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="py-2 px-4 border-b border-gray-500">Proteins</td>
-            <td className="py-2 px-4 border-b border-gray-500">{(weight*2)} gm</td>
-          </tr>
-          <tr>
-            <td className="py-2 px-4 border-b border-gray-500">Carbs</td>
-            <td className="py-2 px-4 border-b border-gray-500">{Math.round((calories-((weight*8)+ (calories*0.25)))/4)} gm</td>
-          </tr>
-          <tr>
-            <td className="py-2 px-4 border-b border-gray-500">Fats</td>
-            <td className="py-2 px-4 border-b border-gray-500"> {Math.round(calories/36)} gm</td>
-          </tr>
-        </tbody>
-      </table>
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border-b border-gray-500">Macro</th>
+              <th className="py-2 px-4 border-b border-gray-500">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="py-2 px-4 border-b border-gray-500">Proteins</td>
+              <td className="py-2 px-4 border-b border-gray-500">
+                {weight * 2} gm
+              </td>
+            </tr>
+            <tr>
+              <td className="py-2 px-4 border-b border-gray-500">Carbs</td>
+              <td className="py-2 px-4 border-b border-gray-500">
+                {Math.round((calories - (weight * 8 + calories * 0.25)) / 4)} gm
+              </td>
+            </tr>
+            <tr>
+              <td className="py-2 px-4 border-b border-gray-500">Fats</td>
+              <td className="py-2 px-4 border-b border-gray-500">
+                {" "}
+                {Math.round(calories / 36)} gm
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
