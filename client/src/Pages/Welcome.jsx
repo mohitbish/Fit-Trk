d9c9466bf85dcad0b5dd160ec2f2161fbbfd6455
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-07-11 02:46:27 +1000

    logo update

--- client/src/Pages/Welcome.jsx
+++ client/src/Pages/Welcome.jsx
@@ -2,32 +2,33 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Register from "../Components/Register";
 import Login from "../Components/Login";
-
+import logo from "../Assests/1.png"
 
 const Welcome = () => {
   const [logincheck, setlogincheck] = useState(false);
-  const changelogincheck = ()=>{
-    setlogincheck(!logincheck)
-  }
+  const changelogincheck = () => {
+    setlogincheck(!logincheck);
+  };
 
   return (
     <div className="w-[100vw] h-[100vh] bg-[#202124]">
       <div className=" h-[10vh] flex flex-row space justify-between px-5 sm:px-10   text-white border-2 border-gray-500">
-        <Link
-          className="sm:text-xl py-4 sm:py-5 py-x sm:px-10 font-bond uppercase hover:bg-[#323639]"
-          to="/"
-        >
-          fit-trk
-        </Link>
-
+        <img
+          src={logo}
+          alt="Girl in a jacket"
+          width="100"
+          height="80"
+        />
         {logincheck ? (
-          <Link className="sm:text-xl py-4 sm:py-5 py-x sm:px-10 font-bond uppercase underline hover:bg-[#323639]"
-          onClick={changelogincheck}>
+          <Link
+            className="sm:text-xl py-4 sm:py-5 py-x sm:px-10 mt-5 sm:mt-0 font-bond uppercase underline hover:bg-[#323639]"
+            onClick={changelogincheck}
+          >
             sign-up
           </Link>
         ) : (
           <Link
-            className="sm:text-xl py-4 sm:py-5 py-x sm:px-10 font-bond uppercase underline hover:bg-[#323639]"
+            className="sm:text-xl py-4 sm:py-5 py-x sm:px-10 mt-5  sm:mt-0 font-bond uppercase underline hover:bg-[#323639]"
             onClick={changelogincheck}
           >
             Log In
@@ -35,13 +36,12 @@ const Welcome = () => {
         )}
       </div>
       <div className=" w-full h-[90vh] flex flex-col sm:flex-row space justify-between px-5 sm:px-10 py-10 sm:py-15  text-white border-2 border-gray-500">
-        <p className=" sm:w-2/5 h-2/5 sm:h-full  aling-centre">
+        <p className=" sm:w-3/5 h-2/5 sm:h-full  aling-centre">
           Some welcome text add passport js auth down in row for md and icons
           for sm
         </p>
-        <div className=" sm:w-3/5 h-3/5 sm:h-full mx-2 items-center">
-          
-          {logincheck?(<Login />):(<Register />)}
+        <div className=" sm:w-2/5 h-3/5 sm:h-full mx-2 items-center">
+          {logincheck ? <Login /> : <Register />}
         </div>
       </div>
     </div>
