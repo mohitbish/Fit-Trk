86fdd8dc0997971e8f1384fb0036c6888422eb00
Mohit Bishnoi <bishnoi.mohit@icloud.com>
2023-07-13 03:18:44 +1000

    work on home page css

--- client/src/Pages/Welcome.jsx
+++ client/src/Pages/Welcome.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Register from "../Components/Register";
 import Login from "../Components/Login";
-import logo from "../Assests/1.png"
+import logo from "../Assests/1.png";
 
 const Welcome = () => {
   const [logincheck, setlogincheck] = useState(false);
@@ -13,12 +13,7 @@ const Welcome = () => {
   return (
     <div className="w-[100vw] h-[100vh] bg-[#202124]">
       <div className=" h-[10vh] flex flex-row space justify-between px-5 sm:px-10   text-white border-2 border-gray-500">
-        <img
-          src={logo}
-          alt="Girl in a jacket"
-          width="100"
-          height="80"
-        />
+        <img src={logo} alt="Girl in a jacket" width="100" height="80" />
         {logincheck ? (
           <Link
             className="sm:text-xl py-4 sm:py-5 py-x sm:px-10 mt-5 sm:mt-0 font-bond uppercase underline hover:bg-[#323639]"
@@ -35,11 +30,23 @@ const Welcome = () => {
           </Link>
         )}
       </div>
-      <div className=" w-full h-[90vh] flex flex-col sm:flex-row space justify-between px-5 sm:px-10 py-10 sm:py-15  text-white border-2 border-gray-500">
-        <p className=" sm:w-1/2 h-2/5 sm:h-full  aling-centre">
-          Some welcome text add passport js auth down in row for md and icons
-          for sm
-        </p>
+      <div className=" w-full h-[90vh] flex flex-col sm:flex-row space justify-between px-5 sm:px-10 py-10 sm:py-15 my-10 text-white ">
+        <div>
+          <p className=" text-2xl font-bold text-center uppercase">
+            welcome to fit-trk
+          </p>
+          <p className=" text uppercase text-center  mt-2 mb-10">
+            [workouts and diet managing tool]
+          </p>
+          <p className=" uppercase my-1"> # Create your own workout-split</p>
+          <p className=" uppercase my-1">
+            # Structure daily workouts to your personal needs
+          </p>
+          <p className=" uppercase my-1">
+            # Get calorie and macro data for your goals
+          </p>
+        </div>
+
         <div className=" sm:w-1/2 h-3/5 sm:h-full mx-2 items-center">
           {logincheck ? <Login /> : <Register />}
         </div>
